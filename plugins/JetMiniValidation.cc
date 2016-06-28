2e9d96c389d4e766fab35dfbefd75dee401bb82a
James <jdolen@fnal.gov>
2016-06-28 12:36:04 -0500

    fixing jmeMiniValidation

--- plugins/JetMiniValidation.cc
+++ plugins/JetMiniValidation.cc
@@ -122,18 +122,21 @@ class JetMiniValidation : public edm::one::EDAnalyzer<edm::one::SharedResources>
       TH1D * h_deltaR_chs_puppi      ;
 
       TTree *  ak4JetTree;
-      Float_t ak4_pt_uncorr;
-      Float_t ak4_pt;
-      Float_t ak4_eta;
-      Float_t ak4_puid;
-      Float_t ak4_flavHadron;
-      Float_t ak4_flavParton;
-      Float_t ak4_ptGen;
-      Float_t ak4_ptResponse;
-      Float_t ak4_dRminGen;
+      Float_t  ak4_pt_uncorr;
+      Float_t  ak4_pt;
+      Float_t  ak4_eta;
+      Float_t  ak4_puid;
+      Float_t  ak4_flavHadron;
+      Float_t  ak4_flavParton;
+      Float_t  ak4_ptGen;
+      Float_t  ak4_ptResponse;
+      Float_t  ak4_dRminGen;
+      Float_t  ak4_area;
+      Float_t  ak4_rho;
+      Int_t    ak4_nvtx;
 
-      TTree *  ak8JetTree;
 
+      TTree * ak8JetTree;
       Float_t ak8_pt                  ;
       Float_t ak8_mass                ;
       Float_t ak8_rapidity            ;
@@ -146,7 +149,7 @@ class JetMiniValidation : public edm::one::EDAnalyzer<edm::one::SharedResources>
       Float_t ak8_tau21               ;
       Float_t ak8_ndau                ;
       Float_t ak8_subjet_M            ;
-      Float_t ak8_subjet_count            ;
+      Float_t ak8_subjet_count        ;
       Float_t ak8_puppi_pt            ;
       Float_t ak8_puppi_mass          ;
       Float_t ak8_puppi_softDropMass  ;
@@ -156,11 +159,14 @@ class JetMiniValidation : public edm::one::EDAnalyzer<edm::one::SharedResources>
       Float_t ak8_puppi_tau32         ;
       Float_t ak8_puppi_tau21         ;
       Float_t ak8_puppi_subjet_M      ;
-      Float_t ak8_puppi_subjet_count      ;
+      Float_t ak8_puppi_subjet_count  ;
       Float_t ak8_mGen                ;
       Float_t ak8_ptGen               ;
       Float_t ak8_ptResponse          ;
       Float_t ak8_dRminGen            ;
+      Float_t ak8_area                ;
+      Float_t ak8_rho                 ;
+      Int_t   ak8_nvtx                ;
 
 };
 
@@ -175,8 +181,8 @@ JetMiniValidation::JetMiniValidation(const edm::ParameterSet& iConfig):
     ak8genjetToken_(consumes<reco::GenJetCollection>(edm::InputTag("slimmedGenJetsAK8"))),
     prunedGenToken_(consumes<edm::View<reco::GenParticle> >(edm::InputTag("prunedGenParticles"))),
     rhoToken_(consumes<double>(edm::InputTag("fixedGridRhoFastjetAll"))),
-    vtxToken_(consumes<std::vector<reco::Vertex> >(edm::InputTag("offlineSlimmedPrimaryVertices")))
-    //jecPayloads_        (iConfig.getParameter<std::vector<std::string> >  ("jecPayloads"))
+    vtxToken_(consumes<std::vector<reco::Vertex> >(edm::InputTag("offlineSlimmedPrimaryVertices"))),
+    jecPayloads_        (iConfig.getParameter<std::vector<std::string> >  ("jecPayloads"))
 {
   usesResource("TFileService");
   edm::Service<TFileService> fs;
@@ -230,37 +236,43 @@ JetMiniValidation::JetMiniValidation(const edm::ParameterSet& iConfig):
   ak4JetTree->Branch("ak4_ptGen"            , & ak4_ptGen          , "ak4_ptGen/F"            );          
   ak4JetTree->Branch("ak4_ptResponse"       , & ak4_ptResponse     , "ak4_ptResponse/F"       );          
   ak4JetTree->Branch("ak4_dRminGen"         , & ak4_dRminGen       , "ak4_dRminGen/F"         );          
+  ak4JetTree->Branch("ak4_area"             , & ak4_area           , "ak4_area/F"             );          
+  ak4JetTree->Branch("ak4_rho"              , & ak4_rho            , "ak4_rho/F"              );          
+  ak4JetTree->Branch("ak4_nvtx"             , & ak4_nvtx           , "ak4_nvtx/I"             );          
 
 
   ak8JetTree = new TTree("ak8JetTree","ak8JetTree");  
-  ak8JetTree->Branch("ak8_pt"                 , & ak8_pt                 , "ak8_pt/F"                 );          
-  ak8JetTree->Branch("ak8_mass"               , & ak8_mass               , "ak8_mass/F"               );          
-  ak8JetTree->Branch("ak8_rapidity"           , & ak8_rapidity           , "ak8_rapidity/F"           );          
-  ak8JetTree->Branch("ak8_prunedMass"         , & ak8_prunedMass         , "ak8_prunedMass/F"         );          
-  ak8JetTree->Branch("ak8_sdMass"             , & ak8_sdMass             , "ak8_sdMass/F"             );          
-  ak8JetTree->Branch("ak8_tau1"               , & ak8_tau1               , "ak8_tau1/F"               );          
-  ak8JetTree->Branch("ak8_tau2"               , & ak8_tau2               , "ak8_tau2/F"               );          
-  ak8JetTree->Branch("ak8_tau3"               , & ak8_tau3               , "ak8_tau3/F"               );          
-  ak8JetTree->Branch("ak8_tau32"              , & ak8_tau32              , "ak8_tau32/F"              );          
-  ak8JetTree->Branch("ak8_tau21"              , & ak8_tau21              , "ak8_tau21/F"              );          
-  ak8JetTree->Branch("ak8_ndau"               , & ak8_ndau               , "ak8_ndau/F"               );          
-  ak8JetTree->Branch("ak8_subjet_M"           , & ak8_subjet_M           , "ak8_subjet_M/F"           );          
-  ak8JetTree->Branch("ak8_subjet_count"           , & ak8_subjet_count           , "ak8_subjet_count/F"           );          
-  ak8JetTree->Branch("ak8_puppi_pt"           , & ak8_puppi_pt           , "ak8_puppi_pt/F"           );          
-  ak8JetTree->Branch("ak8_puppi_mass"         , & ak8_puppi_mass         , "ak8_puppi_mass/F"         );          
-  ak8JetTree->Branch("ak8_puppi_softDropMass" , & ak8_puppi_softDropMass , "ak8_puppi_softDropMass/F" );          
-  ak8JetTree->Branch("ak8_puppi_tau1"         , & ak8_puppi_tau1         , "ak8_puppi_tau1/F"         );          
-  ak8JetTree->Branch("ak8_puppi_tau2"         , & ak8_puppi_tau2         , "ak8_puppi_tau2/F"         );          
-  ak8JetTree->Branch("ak8_puppi_tau3"         , & ak8_puppi_tau3         , "ak8_puppi_tau3/F"         );          
-  ak8JetTree->Branch("ak8_puppi_tau32"        , & ak8_puppi_tau32        , "ak8_puppi_tau32/F"        );          
-  ak8JetTree->Branch("ak8_puppi_tau21"        , & ak8_puppi_tau21        , "ak8_puppi_tau21/F"        );          
-  ak8JetTree->Branch("ak8_puppi_subjet_M"     , & ak8_puppi_subjet_M     , "ak8_puppi_subjet_M/F"     );          
-  ak8JetTree->Branch("ak8_puppi_subjet_count"     , & ak8_puppi_subjet_count     , "ak8_puppi_subjet_count/F"     );          
-  
-  ak8JetTree->Branch("ak8_mGen"               , & ak8_mGen              , "ak8_mGen/F"              );    
-  ak8JetTree->Branch("ak8_ptGen"              , & ak8_ptGen              , "ak8_ptGen/F"              );    
-  ak8JetTree->Branch("ak8_ptResponse"         , & ak8_ptResponse         , "ak8_ptResponse/F"         );    
-  ak8JetTree->Branch("ak8_dRminGen"           , & ak8_dRminGen           , "ak8_dRminGen/F"           );    
+  ak8JetTree->Branch("ak8_pt"                   , & ak8_pt                   , "ak8_pt/F"                 );          
+  ak8JetTree->Branch("ak8_mass"                 , & ak8_mass                 , "ak8_mass/F"               );          
+  ak8JetTree->Branch("ak8_rapidity"             , & ak8_rapidity             , "ak8_rapidity/F"           );          
+  ak8JetTree->Branch("ak8_prunedMass"           , & ak8_prunedMass           , "ak8_prunedMass/F"         );          
+  ak8JetTree->Branch("ak8_sdMass"               , & ak8_sdMass               , "ak8_sdMass/F"             );          
+  ak8JetTree->Branch("ak8_tau1"                 , & ak8_tau1                 , "ak8_tau1/F"               );          
+  ak8JetTree->Branch("ak8_tau2"                 , & ak8_tau2                 , "ak8_tau2/F"               );          
+  ak8JetTree->Branch("ak8_tau3"                 , & ak8_tau3                 , "ak8_tau3/F"               );          
+  ak8JetTree->Branch("ak8_tau32"                , & ak8_tau32                , "ak8_tau32/F"              );          
+  ak8JetTree->Branch("ak8_tau21"                , & ak8_tau21                , "ak8_tau21/F"              );          
+  ak8JetTree->Branch("ak8_ndau"                 , & ak8_ndau                 , "ak8_ndau/F"               );          
+  ak8JetTree->Branch("ak8_subjet_M"             , & ak8_subjet_M             , "ak8_subjet_M/F"           );          
+  ak8JetTree->Branch("ak8_subjet_count"         , & ak8_subjet_count         , "ak8_subjet_count/F"       );          
+  ak8JetTree->Branch("ak8_puppi_pt"             , & ak8_puppi_pt             , "ak8_puppi_pt/F"           );          
+  ak8JetTree->Branch("ak8_puppi_mass"           , & ak8_puppi_mass           , "ak8_puppi_mass/F"         );          
+  ak8JetTree->Branch("ak8_puppi_softDropMass"   , & ak8_puppi_softDropMass   , "ak8_puppi_softDropMass/F" );          
+  ak8JetTree->Branch("ak8_puppi_tau1"           , & ak8_puppi_tau1           , "ak8_puppi_tau1/F"         );          
+  ak8JetTree->Branch("ak8_puppi_tau2"           , & ak8_puppi_tau2           , "ak8_puppi_tau2/F"         );          
+  ak8JetTree->Branch("ak8_puppi_tau3"           , & ak8_puppi_tau3           , "ak8_puppi_tau3/F"         );          
+  ak8JetTree->Branch("ak8_puppi_tau32"          , & ak8_puppi_tau32          , "ak8_puppi_tau32/F"        );          
+  ak8JetTree->Branch("ak8_puppi_tau21"          , & ak8_puppi_tau21          , "ak8_puppi_tau21/F"        );          
+  ak8JetTree->Branch("ak8_puppi_subjet_M"       , & ak8_puppi_subjet_M       , "ak8_puppi_subjet_M/F"     );          
+  ak8JetTree->Branch("ak8_puppi_subjet_count"   , & ak8_puppi_subjet_count   , "ak8_puppi_subjet_count/F" );          
+  ak8JetTree->Branch("ak8_area"                 , & ak8_area                 , "ak8_area/F"               );          
+  ak8JetTree->Branch("ak8_rho"                  , & ak8_rho                  , "ak8_rho/F"                );          
+  ak8JetTree->Branch("ak8_nvtx"                 , & ak8_nvtx                 , "ak8_nvtx/F"               );          
+
+  ak8JetTree->Branch("ak8_mGen"                 , & ak8_mGen                 , "ak8_mGen/F"               );    
+  ak8JetTree->Branch("ak8_ptGen"                , & ak8_ptGen                , "ak8_ptGen/F"              );    
+  ak8JetTree->Branch("ak8_ptResponse"           , & ak8_ptResponse           , "ak8_ptResponse/F"         );    
+  ak8JetTree->Branch("ak8_dRminGen"             , & ak8_dRminGen             , "ak8_dRminGen/F"           );    
 
  
 }
@@ -284,12 +296,27 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
   using namespace reco;
   using namespace pat;
 
+  bool verbose = false;
+
+  // Vertices
+  edm::Handle<std::vector<reco::Vertex> > vertices;
+  iEvent.getByToken(vtxToken_, vertices);
+  int nvtx = vertices->size();
+
+  // Rho
+  Handle<double> rhoH;
+  iEvent.getByToken(rhoToken_, rhoH);
+  double rho = *rhoH;
+
+  // AK4 jet loop from miniAOD
+  
+  edm::Handle<pat::JetCollection> AK4CHS;
+  iEvent.getByToken(ak4jetToken_, AK4CHS);
 
   edm::Handle<reco::GenJetCollection> AK4GENJET;  
   iEvent.getByToken(ak4genjetToken_, AK4GENJET);
   
-  edm::Handle<pat::JetCollection> AK4CHS;
-  iEvent.getByToken(ak4jetToken_, AK4CHS);
+
   for (const pat::Jet &ijet : *AK4CHS) {  
     double pt           = ijet.pt();
     double pt_uncorr    = ijet.pt()*ijet.jecFactor("Uncorrected");
@@ -297,8 +324,22 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
     double phi          = ijet.phi();
     double ndau         = ijet.numberOfDaughters();
     double puid         = ijet.userFloat("pileupJetId:fullDiscriminant");
-    // double bdisc_CSV        = ijet.bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags");
-    // double bdisc_MVA        = ijet.bDiscriminator("pfCombinedMVAV2BJetTags");
+
+    // jet ID
+    double NHF       = ijet.neutralHadronEnergyFraction();
+    double NEMF      = ijet.neutralEmEnergyFraction();
+    double CHF       = ijet.chargedHadronEnergyFraction();
+    // double MUF       = ijet.muonEnergyFraction();
+    double CEMF      = ijet.chargedEmEnergyFraction();
+    // double NumConst  = ijet.chargedMultiplicity()+ijet.neutralMultiplicity();
+    double NM        = ijet.neutralMultiplicity();
+    double CM        = ijet.chargedMultiplicity(); 
+
+    bool goodJet_looseJetID =  
+      (fabs(eta) < 2.4 && CHF > 0.0 && NHF < 0.99 && CM > 0 && CEMF < 0.99 && NEMF < 0.99) 
+      || ( fabs(eta) >= 2.4 && fabs(eta) < 3.0 && NEMF < 0.9 && NM > 2 ) 
+      || ( fabs(eta) >= 3.0 && NEMF < 0.9 && NM > 10 );
+    if (verbose) cout<<"goodJet = "<<goodJet_looseJetID<<endl;
 
     h_ak4chs_pt         ->Fill( pt         );
     h_ak4chs_eta        ->Fill( eta        );
@@ -334,15 +375,22 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
     ak4_eta         =  eta;
     ak4_puid        =  puid;
 
+    ak4_area     = ijet.jetArea();
+    ak4_rho      = rho  ;
+    ak4_nvtx     = nvtx ;
+
     ak4JetTree->Fill();
 
   }
 
+  // ak8 jet Loop
+ 
+  edm::Handle<pat::JetCollection> AK8MINI;
+  iEvent.getByToken(ak8jetToken_, AK8MINI);
+
   edm::Handle<reco::GenJetCollection> AK8GENJET;  
   iEvent.getByToken(ak8genjetToken_, AK8GENJET);
 
-  edm::Handle<pat::JetCollection> AK8MINI;
-  iEvent.getByToken(ak8jetToken_, AK8MINI);
   for (const pat::Jet &ijet : *AK8MINI) {  
     double pt           = ijet.pt();
     if (pt<200) continue;
@@ -386,7 +434,6 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
     double mostMassiveSDsubjetMass = 0;
     int count_SD =0;
     auto const & sdSubjets = ijet.subjets("SoftDrop");
-    cout<<"sdSubjets size "<<sdSubjets.size()<<endl;
     for ( auto const & it : sdSubjets ) {
       double subjetPt       = it->pt();
       double subjetPtUncorr = it->pt();
@@ -395,7 +442,7 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
       double subjetMass     = it->mass();
       double subjetBdisc    = it->bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags"); 
       double deltaRsubjetJet = deltaR(ijet.eta(), ijet.phi(), subjetEta, subjetPhi);
-      cout<<" SD Subjet pt "<<subjetPt<<" uncorr "<<subjetPtUncorr<<" Eta "<<subjetEta<<" deltaRsubjetJet "<<deltaRsubjetJet<<" Mass "<<subjetMass<<" Bdisc "<<subjetBdisc<<endl;
+      if (verbose) cout<<" SD Subjet pt "<<subjetPt<<" uncorr "<<subjetPtUncorr<<" Eta "<<subjetEta<<" deltaRsubjetJet "<<deltaRsubjetJet<<" Mass "<<subjetMass<<" Bdisc "<<subjetBdisc<<endl;
       h_ak8chs_sdSubjetMass ->Fill( subjetMass );
       if (subjetMass > mostMassiveSDsubjetMass) mostMassiveSDsubjetMass = subjetMass;
       count_SD++;
@@ -406,7 +453,6 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
     TLorentzVector pup1;
     double mostMassiveSDPUPPIsubjetMass = 0;
     auto const & sdSubjetsPuppi = ijet.subjets("SoftDropPuppi");
-    cout<<"sdSubjetsPuppi size "<<sdSubjetsPuppi.size()<<endl;
     int count_pup=0;
     for ( auto const & it : sdSubjetsPuppi ) {
       double subjetPt       = it->pt();
@@ -416,14 +462,14 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
       double subjetMass     = it->mass();
       double subjetBdisc    = it->bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags"); 
       double deltaRsubjetJet = deltaR(ijet.eta(), ijet.phi(), subjetEta, subjetPhi);
-      cout<<" SD Subjet pt "<<subjetPt<<" uncorr "<<subjetPtUncorr<<" Eta "<<subjetEta<<" deltaRsubjetJet "<<deltaRsubjetJet<<" Mass "<<subjetMass<<" Bdisc "<<subjetBdisc<<endl; 
+      if (verbose) cout<<" SD Subjet pt "<<subjetPt<<" uncorr "<<subjetPtUncorr<<" Eta "<<subjetEta<<" deltaRsubjetJet "<<deltaRsubjetJet<<" Mass "<<subjetMass<<" Bdisc "<<subjetBdisc<<endl; 
       h_ak8puppi_sdSubjetMass ->Fill( subjetMass );
       if (count_pup==0) pup0.SetPtEtaPhiM( subjetPt, subjetEta, subjetPhi, subjetMass);
       if (count_pup==1) pup1.SetPtEtaPhiM( subjetPt, subjetEta, subjetPhi, subjetMass);
       if (subjetMass > mostMassiveSDPUPPIsubjetMass) mostMassiveSDPUPPIsubjetMass = subjetMass;
       count_pup++;
     }
-    cout<<"count_pup "<<count_pup<<endl;
+
     TLorentzVector puppiSD;
     if (count_pup>1)
     {
@@ -439,8 +485,6 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
     h_ak8chs_mass         ->Fill( mass         );
     h_ak8chs_rapidity     ->Fill( rapidity     );
     h_ak8chs_prunedMass   ->Fill( prunedMass   );
-    // h_ak8chs_trimmedMass  ->Fill( trimmedMass  );
-    // h_ak8chs_filteredMass ->Fill( filteredMass );
     h_ak8chs_softDropMass ->Fill( softDropMass );
     h_ak8chs_tau1         ->Fill( tau1         );
     h_ak8chs_tau2         ->Fill( tau2         );
@@ -486,6 +530,10 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
     ak8_puppi_subjet_M     = mostMassiveSDPUPPIsubjetMass ;
     ak8_puppi_subjet_count     = count_pup ;
 
+    ak8_area     = ijet.jetArea();
+    ak8_rho      = rho  ;
+    ak8_nvtx     = nvtx ;
+
     if (!iEvent.isRealData()) {
       float dRmin = 100.;
       float ptGen = -1.0;
@@ -498,15 +546,7 @@ JetMiniValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
           mGen = igen.mass();
         }
       }
-      // h_ak4chs_dRminGen         ->Fill( dRmin       );
-      // h_ak4chs_ptGen            ->Fill( ptGen       );
-      // if (ptGen>0) h_ak4chs_ptResponse       ->Fill( pt/ptGen );
-      // h_ak4chs_flavParton       ->Fill( ijet.partonFlavour()       );
-      // h_ak4chs_flavHadron       ->Fill( ijet.hadronFlavour()       );
-      
-      // ak4_flavParton  =  ijet.partonFlavour()  ;
-      // ak4_flavHadron  =  ijet.hadronFlavour()  ;
-      ak8_mGen       =  mGen;
+      ak8_mGen        =  mGen;
       ak8_ptGen       =  ptGen;
       ak8_ptResponse  =  pt/ptGen ;
       ak8_dRminGen    =  dRmin;
