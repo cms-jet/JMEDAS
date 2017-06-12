37f51467a2b2891ce9f3e0852749303beb0a0fa6
Justin Pilot <justin.pilot@cern.ch>
2017-06-12 14:33:48 -0500

    Updated plots4

--- test/plots4.py
+++ test/plots4.py
@@ -25,11 +25,11 @@ leg.AddEntry( h_msoftdropAK8, "Soft Drop", 'l')
 leg.AddEntry( h_mprunedAK8, "Pruned", 'l')
 
 c = TCanvas('c', 'c')
-h_mAK8.Draw()
+h_mprunedAK8.Draw()
 h_msoftdropAK8   .Draw("same") 
-h_mprunedAK8     .Draw("same") 
-h_mAK8.GetXaxis().SetRangeUser(0, 200)
-h_mAK8.SetMaximum( 200 )
+h_mAK8     .Draw("same") 
+h_mAK8.GetXaxis().SetRangeUser(0, 250)
+h_mprunedAK8.SetMaximum( h_msoftdropAK8.GetMaximum()*1.2 )
 
 leg.Draw()
 
