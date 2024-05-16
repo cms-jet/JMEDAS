5ffc0d23d2c1b0bf1f70403f67252a0d8de71569
Huilin Qu <huilin.qu@cern.ch>
2024-05-16 15:45:59 +0200

    Remove unused files.

--- python/convertPackedCandToRecoCand_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-convertedPackedPFCandidates = cms.EDProducer('convertPackedCandToRecoCand',
-       								     	  src = cms.InputTag('packedPFCandidates')
-            							  	)
